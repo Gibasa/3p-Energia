851398bc2961a646a44cbfb1f4baa9f86b30572d
Gilberto Sá <gibasa@gmail.com>
2023-07-28 10:40:36 -0300

    deploy config done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
   plugins: [reactRefresh()],
+  base:"/3p-Energia",
   esbuild: {
     loader: 'jsx',
     jsxInject: 'import React from "react"'
