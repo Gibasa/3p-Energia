f09e111240fa5dc1d96b7afc37426e5b13d3265a
Gilberto Sá <gibasa@gmail.com>
2023-08-25 15:47:51 -0300

    header and responsivity updated.

--- src/assets/components/layout/home/Home.jsx
+++ src/assets/components/layout/home/Home.jsx
@@ -10,7 +10,7 @@ import CarouselComponent from "./Carousel"
 
 function Home(){
     return(
-        <div>
+        <div className="home">
             <Banner />
             <Servicos/>
             <QuemSomos/>
