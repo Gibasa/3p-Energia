dc2bca9443d80b53528ff11bbaa62d6c001a36d8
Gibasa <gibasa@gmail.com>
2026-03-10 11:50:16 -0300

    Quem somos font weight updated

--- src/assets/components/layout/home/QuemSomos.jsx
+++ src/assets/components/layout/home/QuemSomos.jsx
@@ -6,18 +6,17 @@ function QuemSomos() {
       <img src="./images/HOME QUEM SOMOS.png" alt="" loading="lazy" />
       <div className="text">
         <h2>Quem Somos</h2>
-        <p>Propósito, Performance, Preservação</p>
         <p>
-          Com <strong>10 anos de atuação no setor de elétrico, a 3P Energia entrega soluções completas e integradas</strong> em <strong>geração, transmissão, distribuição e armazenamento de energia</strong>.
+          Com <strong>10 anos de atuação</strong> no <strong>setor de elétrico</strong>, a <strong>3P Energia</strong> entrega <strong>soluções completas e integradas</strong> em <strong>geração, transmissão, distribuição</strong> e <strong>armazenamento de energia</strong>.
         </p>
         <p>
-          Atuamos além da <strong>instalação</strong>, com <strong>engenharia aplicada, implantação, comissionamento, documentação técnica</strong> e <strong>suporte especializado</strong> para garantir <strong>desempenho, segurança e conformidade regulatória</strong>.
+          Atuamos além da <strong>instalação</strong>, com <strong>engenharia aplicada, implantação, comissionamento, documentação técnica</strong> e <strong>suporte especializado</strong> para garantir <strong>desempenho, segurança</strong> e <strong>conformidade regulatória</strong>.
         </p>
         <p>
           Nosso foco é atender <strong>grandes corporações</strong> com <strong>projetos de infraestrutura de energia</strong> que geram <strong>resultados mensuráveis</strong>, como <strong>redução de custos operacionais, aumento de confiabilidade elétrica</strong> e avanço nas <strong>metas de sustentabilidade</strong>.
         </p>
         <p>
-          A <strong>3P Energia é parceira estratégica na transição energética</strong>, conectando <strong>eficiência técnica</strong> a <strong>benefícios reais</strong> para o negócio.
+          A <strong>3P Energia</strong> é <strong>parceira estratégica</strong> na <strong>transição energética</strong>, conectando <strong>eficiência técnica</strong> a <strong>benefícios reais</strong> para o negócio.
         </p>
       </div>
     </div>
